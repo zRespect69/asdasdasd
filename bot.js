628f8a3a997e770f547227bd3c0532dbf8fd63d3
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-22 20:26:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,22 +14,22 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
     
-  if (message.content.startsWith('#p')) 
+  if (message.content.startsWith('#ply')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
      if (message.content === ("leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith('#w')) {
+  if (message.content.startsWith('#wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('#l')) {
+  if (message.content.startsWith('#ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('#s')) {
+  if (message.content.startsWith('#st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
