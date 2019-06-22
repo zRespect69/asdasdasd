4c8321da3787629d5d2e90cc139fc0698f6677fa
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-22 20:13:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,26 +7,6 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-  if (message.content.startsWith("#avatar")) {
-if(!message.channel.guild) return;
-      var mentionned = message.mentions.users.first();
-  var client;
-    if(mentionned){
-        var client = mentionned; } else {
-        var client = message.author;
-    }
-      const embed = new Discord.RichEmbed()
-                         .addField('Requested by:', "<@" + message.author.id + ">")
-      .setColor(000000)
-      .setImage(`${client.avatarURL}`)
-    message.channel.sendEmbed(embed);
-  }
-});
-
-
-
-
 
 const devs = ["502799582045011970"];
 const adminprefix = ["#"];
